54b805da5f6b7946d6ecec64d1861a7d73528b72
gitlfid <github@linksfield.id>
2026-03-04 13:38:23 +0700

    CRM delivery order : update

--- admin/delivery_order_print.php
+++ admin/delivery_order_print.php
@@ -172,10 +172,6 @@ while($row = $res->fetch_assoc()) $sets[$row['setting_key']] = $row['setting_val
                             <td><div contenteditable="true"><?= htmlspecialchars($display_pic ?? '') ?></div></td>
                         </tr>
                         <tr>
-                            <td class="lbl">Contact</td><td class="sep">:</td>
-                            <td><div contenteditable="true"><?= htmlspecialchars($display_pic ?? '') ?></div></td>
-                        </tr>
-                        <tr>
                             <td class="lbl">Phone</td><td class="sep">:</td>
                             <td><div contenteditable="true"><?= htmlspecialchars($display_phone ?? '') ?></div></td>
                         </tr>
@@ -191,6 +187,10 @@ while($row = $res->fetch_assoc()) $sets[$row['setting_key']] = $row['setting_val
                         <tr>
                             <td class="lbl">Delivery No</td><td class="sep">:</td>
                             <td><strong contenteditable="true"><?= $do['do_number'] ?? '-' ?></strong></td>
+                        </tr>
+                         <tr>
+                            <td class="lbl">Contact</td><td class="sep">:</td>
+                            <td><div contenteditable="true"><?= htmlspecialchars($display_pic ?? '') ?></div></td>
                         </tr>
                     </table>
                 </td>
