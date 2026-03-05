824d30c8d4c29538182879736cbab1c5f379de66
gitlfid <github@linksfield.id>
2026-03-05 11:21:36 +0700

    invoice print : update

--- admin/invoice_print.php
+++ admin/invoice_print.php
@@ -103,6 +103,94 @@ function format_money($num, $is_intl) {
         return number_format((float)$num, 0, ',', '.');
     }
 }
+
+// FUNGSI KONVERSI ANGKA KE HURUF (TERBILANG BAHASA INGGRIS)
+function getSpelledOutNumber($number) {
+    $number = str_replace(',', '', $number);
+    $hyphen      = '-';
+    $conjunction = ' ';
+    $separator   = ' ';
+    $negative    = 'Negative ';
+    $dictionary  = array(
+        0                   => 'Zero',
+        1                   => 'One',
+        2                   => 'Two',
+        3                   => 'Three',
+        4                   => 'Four',
+        5                   => 'Five',
+        6                   => 'Six',
+        7                   => 'Seven',
+        8                   => 'Eight',
+        9                   => 'Nine',
+        10                  => 'Ten',
+        11                  => 'Eleven',
+        12                  => 'Twelve',
+        13                  => 'Thirteen',
+        14                  => 'Fourteen',
+        15                  => 'Fifteen',
+        16                  => 'Sixteen',
+        17                  => 'Seventeen',
+        18                  => 'Eighteen',
+        19                  => 'Nineteen',
+        20                  => 'Twenty',
+        30                  => 'Thirty',
+        40                  => 'Forty',
+        50                  => 'Fifty',
+        60                  => 'Sixty',
+        70                  => 'Seventy',
+        80                  => 'Eighty',
+        90                  => 'Ninety',
+        100                 => 'Hundred',
+        1000                => 'Thousand',
+        1000000             => 'Million',
+        1000000000          => 'Billion',
+        1000000000000       => 'Trillion'
+    );
+
+    if (!is_numeric($number)) return false;
+    if ($number < 0) return $negative . getSpelledOutNumber(abs($number));
+    
+    $string = $fraction = null;
+    if (strpos($number, '.') !== false) {
+        list($number, $fraction) = explode('.', $number);
+    }
+
+    switch (true) {
+        case $number < 21:
+            $string = $dictionary[(int)$number];
+            break;
+        case $number < 100:
+            $tens   = ((int) ($number / 10)) * 10;
+            $units  = $number % 10;
+            $string = $dictionary[$tens];
+            if ($units) $string .= $hyphen . $dictionary[$units];
+            break;
+        case $number < 1000:
+            $hundreds  = floor($number / 100);
+            $remainder = $number % 100;
+            $string = $dictionary[$hundreds] . ' ' . $dictionary[100];
+            if ($remainder) $string .= $conjunction . getSpelledOutNumber($remainder);
+            break;
+        default:
+            $baseUnit = pow(1000, floor(log($number, 1000)));
+            $numBaseUnits = (int) ($number / $baseUnit);
+            $remainder = fmod($number, $baseUnit);
+            $string = getSpelledOutNumber($numBaseUnits) . ' ' . $dictionary[$baseUnit];
+            if ($remainder) {
+                $string .= $remainder < 100 ? $conjunction : $separator;
+                $string .= getSpelledOutNumber($remainder);
+            }
+            break;
+    }
+
+    if (null !== $fraction && is_numeric($fraction)) {
+        $fraction = substr($fraction, 0, 2);
+        if((int)$fraction > 0) {
+            $string .= ' and ' . getSpelledOutNumber((int)$fraction) . ' Cents';
+        }
+    }
+    return $string;
+}
 ?>
 
 <!DOCTYPE html>
@@ -254,6 +342,10 @@ function format_money($num, $is_intl) {
                 }
                 
                 $totalInvoice = $grandTotal + $vatAmount;
+                
+                // MENGUBAH TOTAL INVOICE MENJADI TERBILANG
+                $currency_text = $is_international ? ($inv['currency'] == 'USD' ? "US Dollars" : $inv['currency']) : "Rupiah";
+                $amountInWords = ucwords(strtolower(getSpelledOutNumber($totalInvoice))) . " " . $currency_text . " Only";
             ?>
             
             <tr class="summary-row">
@@ -290,6 +382,14 @@ function format_money($num, $is_intl) {
     <table class="footer-layout">
         <tr>
             <td class="footer-left">
+                
+                <div style="font-size: 11px; margin-bottom: 15px;">
+                    <span style="font-weight: bold;">Amount in words :</span><br>
+                    <div <?= $can_edit_note ?> style="font-style: italic; margin-top: 3px; line-height: 1.4; max-width: 90%;">
+                        # <?= htmlspecialchars($amountInWords) ?> #
+                    </div>
+                </div>
+
                 <div style="font-style: italic; font-size: 10px; margin-bottom: 20px;">
                     <strong>Note :</strong><br>
                     <?php if($is_international): ?>
